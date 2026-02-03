73583ce64badb3b740664d5e59cfad972f630b54
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2026-02-03 10:59:48 +0300

    feat: create an actix web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+
+
+async fn post_program() -> impl Responder{
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main()  -> std::io::Result<()>{
+    let addr = ("127.0.0.1", 8080);
+    println!("app is bound to http://{}:{}",addr.0, addr.1);
+    HttpServer::new(|| {
+        App::new().route("/", web::post().to(post_program))
+    }).bind(addr)?
+    .run()
+    .await
+
 }
